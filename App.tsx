bf6891ed5e36bed9bc41c45f4c83d912fc1b514b
ST10449069 <161335940+ST10449069@users.noreply.github.com>
2025-07-31 19:02:55 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello, Koketso!</Text>
       <StatusBar style="auto" />
+      <Text>
+      The most difficult part for me was when I needed to run the app and it didn't want to run.
+      </Text>
+      <Text>
+      I communicated with my lecture on how to fix the issue due to getting errors
+      </Text>
+      <Text> 
+      and we solved the problem and my app started running smoothly.
+      </Text>
     </View>
   );
 }
